d5aa1ac352633b30a3d6054d3acc9c731b304d65
alpine <alpinenn.dev@gmail.com>
2025-02-18 19:58:20 +0900

    change tag

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 
 from dotenv import load_dotenv
 import os
+import sys
 from loguru import logger
 
 env = os.getenv('env', 'dev')
@@ -8,3 +9,4 @@ load_dotenv()
 msg = f'hello world! env is {env}'
 print('print message')
 logger.info(msg)
+sys.exit(0)
